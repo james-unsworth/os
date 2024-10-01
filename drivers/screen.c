572e648fbd5efef8bc7f86d7c40af3cef968bf38
james-unsworth <168010903+james-unsworth@users.noreply.github.com>
2024-10-01 15:31:57 +0100

    Add line scrolling

--- drivers/screen.c
+++ drivers/screen.c
@@ -1,5 +1,6 @@
 #include "screen.h"
 #include "ports.h"
+#include "../kernel/util.h"
 
 /* Declaration of private functions */
 int get_cursor_offset();
@@ -79,6 +80,21 @@ int print_char(char c, int col, int row, char attr) {
         vidmem[offset+1] = attr;
         offset += 2;
     }
+    /* Scroll function 
+    * Copies memory from each row and pastes it to the one above
+    */
+    if (offset >= MAX_COLS * MAX_ROWS * 2) {
+        for (int i = 1; i < MAX_ROWS; i++) {
+            memory_copy(get_offset(0,i) + VIDEO_ADDRESS,
+                        get_offset(0, i-1) + VIDEO_ADDRESS, MAX_COLS * 2);
+            
+        }
+        char *last_line = get_offset(0, MAX_ROWS-1) + VIDEO_ADDRESS;
+        for (int i = 0; i < MAX_COLS*2; i++) last_line[i] = 0;
+
+        offset -= 2 * MAX_COLS;
+    }
+
     set_cursor_offset(offset);
     return offset;
 }
