a3327fd6f0ac0d0f925829322d2e2f73e1e6051e
james-unsworth <168010903+james-unsworth@users.noreply.github.com>
2024-10-02 15:02:57 +0100

    Add comments

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -1,8 +1,10 @@
 section .text
 global _start        
 
+; Allow main function in kernel.c to be called
 extern main          
 
+; Hand control to kernel
 _start:
     call main      
     hlt              
--- kernel/util.c
+++ kernel/util.c
@@ -1,9 +1,11 @@
+// Copy a specific number of bytes from source memory location to destination
 void memory_copy(char *source, char *dest, int nbytes) {
     for (int i = 0; i < nbytes; i++) {
         *(dest+i) = *(source+i);
     }
 }
 
+// Convert integer to ascii character
 void int_to_ascii(int n, char str[]) {
     int i, sign;
     if ((sign = n) < 0) n = -n;
